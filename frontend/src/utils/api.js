ffb365dac6c3f00db3201c9d14ef40a7a22bf6e9
PaviTechDevloper <pavikutty0609@gmail.com>
2026-04-25 19:42:38 +0530

    feat(k8s): add server in nfs

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,3 @@
 const hostname = window.location.hostname;
-export const API_BASE_URL = process.env.REACT_APP_API_URL || `http://${hostname}:5000`;
-export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || `http://${hostname}:5000`;
+export const API_BASE_URL = `http://${hostname}:5000`;
+export const SOCKET_URL = `http://${hostname}:5000`;
